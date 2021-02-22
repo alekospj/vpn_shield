37b117f7d59701ac961109e622869bdc38c18973
alekospj <alekospj@gmail.com>
2021-02-22 10:25:27 +0100

    ok

---
+++ vpn_shield_new.py
@@ -0,0 +1,29 @@
+import time
+import subprocess
+from multiprocessing import Process
+import socket        
+		
+		
+		
+		
+if __name__ == "__main__":	
+
+	
+	ip = socket.gethostbyname(socket.gethostname())
+	program_loc = r"C:\Users\xoxlios\Desktop\uTorrent\uTorrent.exe"
+	
+	def open_program(path_name):
+		return subprocess.Popen(path_name)
+        
+	p = open_program(program_loc) 
+
+	
+
+	while True:
+		if ip == socket.gethostbyname(socket.gethostname()):
+			print('Conected at:',ip)
+			time.sleep(1)
+		else:
+			print('ip changed!')
+			p.terminate()
+			break
